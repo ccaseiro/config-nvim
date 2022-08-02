20670528a99273fc54bcad9015ca2f0e49757dcb
Claudio Caseiro <ccaseiro@gmail.com>
2022-08-02 18:30:49 +0100

    setup which-key.nvim plugin

--- lua/cc/packer.lua
+++ lua/cc/packer.lua
@@ -11,4 +11,15 @@ return require('packer').startup(function(use)
             require('Comment').setup()
         end
     }
+
+    use {
+        "folke/which-key.nvim",
+        config = function()
+            require("which-key").setup {
+                -- your configuration comes here
+                -- or leave it empty to use the default settings
+                -- refer to the configuration section below
+            }
+        end
+    }
 end)
