cf7bc8a820db57d3d2497b2e44bb241b153ee1a2
Claudio Caseiro <ccaseiro@gmail.com>
2022-08-13 12:05:14 +0100

    conf rust lsp

--- lua/cc/init.lua
+++ lua/cc/init.lua
@@ -1,3 +1,8 @@
 require("cc.set")
 require("cc.packer")
 require("cc.remap")
+
+vim.api.nvim_create_autocmd({"BufWritePre"}, {command = "lua vim.lsp.buf.format()", })
+-- vim.api.nvim_create_autocmd({"BufWritePre"}, {pattern = "*.rs", command = "lua vim.lsp.buf.format()", })
+-- vim.api.nvim_create_autocmd("BufWritePre", { pattern="*.rs", callback = function() vim.lsp.buf.format() end })
+
