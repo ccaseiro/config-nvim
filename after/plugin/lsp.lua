c6a330bacfdd37fc90d572847e71bc349fecfed9
Claudio Caseiro <ccaseiro@gmail.com>
2022-08-20 02:21:18 +0100

    conf lsp keymap 'K'

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,2 +1,10 @@
-require'lspconfig'.rust_analyzer.setup{}
+local on_attach = function(client, bufnr)
+    local bufopts = { noremap=true, silent=true, buffer=bufnr }
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+end
+
+require'lspconfig'.rust_analyzer.setup{
+    on_attach = on_attach
+}
 require'lspconfig'.taplo.setup{} -- toml
+
