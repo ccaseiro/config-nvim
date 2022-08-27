a68bc87db26ae2012603f2fd875e74de9265c07c
Claudio Caseiro <ccaseiro@gmail.com>
2022-08-27 02:07:34 +0100

    remap: window

--- lua/cc/remap.lua
+++ lua/cc/remap.lua
@@ -17,7 +17,21 @@ nnoremap("<leader>o-", "<cmd>Ex<CR>")
 
 nnoremap("<leader>qq", "<cmd>qa<CR>")
 
-nnoremap("<leader>wc", "<cmd>close<CR>")
+nnoremap("<leader>wc", "<C-w>c")
+nnoremap("<leader>ws", "<cmd>split<CR>")
+nnoremap("<leader>wv", "<cmd>vsplit<CR>")
+nnoremap("<leader>ww", "<C-w>w")
+nnoremap("<leader>wl", "<C-w>l")
+nnoremap("<leader>wh", "<C-w>h")
+nnoremap("<leader>wk", "<C-w>k")
+nnoremap("<leader>wj", "<C-w>j")
+nnoremap("<leader>wo", "<C-w>o")
+nnoremap("<leader>w=", "<C-w>=")
+-- Go to window 1,2,3,4
+nnoremap("<leader>wn", "1<C-w>w")
+nnoremap("<leader>we", "2<C-w>w")
+nnoremap("<leader>wi", "3<C-w>w")
+nnoremap("<leader>wy", "4<C-w>w")
 
 nnoremap("<leader>.", "<cmd>Telescope find_files<cr>")
 nnoremap("<leader>ff", "<cmd>Telescope find_files<cr>")
