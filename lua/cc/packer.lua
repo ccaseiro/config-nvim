5694cfa40b16ddc73a3c22e03ad646e7003070a0
Claudio Caseiro <ccaseiro@gmail.com>
2022-10-07 11:01:31 +0100

    conf lazyload for FTerm
    diff --git a/lua/cc/packer.lua b/lua/cc/packer.lua
    index 3e62e53..e2e67ae 100644
    --- a/lua/cc/packer.lua
    +++ b/lua/cc/packer.lua
    @@ -73,7 +73,14 @@ return require('packer').startup(function(use)
         })
    
         -- Terminal
    -    use "numToStr/FTerm.nvim"
    +    -- use { "numToStr/FTerm.nvim", cmd = 'FTerm' }
    +    use {
    +        "numToStr/FTerm.nvim",
    +        config = function()
    +            vim.api.nvim_create_user_command('FTermToggle', require('FTerm').toggle, { bang = true })
    +        end,
    +        cmd = 'FTermToggle'
    +    }
    
         -- git
         use { 'TimUntersberger/neogit', requires = 'nvim-lua/plenary.nvim' }

--- lua/cc/packer.lua
+++ lua/cc/packer.lua
@@ -73,7 +73,14 @@ return require('packer').startup(function(use)
     })
 
     -- Terminal
-    use "numToStr/FTerm.nvim"
+    -- use { "numToStr/FTerm.nvim", cmd = 'FTerm' }
+    use { 
+        "numToStr/FTerm.nvim",
+        config = function()
+            vim.api.nvim_create_user_command('FTermToggle', require('FTerm').toggle, { bang = true })
+        end,
+        cmd = 'FTermToggle'
+    }
 
     -- git
     use { 'TimUntersberger/neogit', requires = 'nvim-lua/plenary.nvim' }
