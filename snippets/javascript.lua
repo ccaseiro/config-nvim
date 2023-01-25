214f0c3c2d436f579ae564bbcba108645cc499d3
Claudio Caseiro <ccaseiro@gmail.com>
2023-01-25 09:41:22 +0000

    configure javascript snippets
    diff --git a/snippets/javascript.lua b/snippets/javascript.lua
    index 2521817..79ea3a6 100644
    --- a/snippets/javascript.lua
    +++ b/snippets/javascript.lua
    @@ -11,13 +11,23 @@ local rep = require("luasnip.extras").rep
    
     local snippets, autosnippets = {}, {}
    
    -local lld= s("lld", fmt("console.log(`{}`)", {i(1)}))
    +-- local lld= s("lld", fmt("console.log(`{}`)", {i(1)}))
    +local llt= s("llt", fmt("logger.trace(`{}`)", {i(1)}))
    +local lld= s("lld", fmt("logger.debug(`{}`)", {i(1)}))
    +local lli= s("lli", fmt("logger.info(`{}`)", {i(1)}))
    +local llw= s("llw", fmt("logger.warn(`{}`)", {i(1)}))
    +local lle= s("lle", fmt("logger.error(`{}`)", {i(1)}))
    +local llf= s("llf", fmt("logger.fatal(`{}`)", {i(1)}))
     local llv= s("llv", fmt("console.log(`{}: ${{{}}}`)", {i(1), rep(1)}))
    -local lli= s("lli", fmt("console.log(`{}: ${{inspect({})}}`)", {i(1), rep(1)}))
    +-- local lli= s("lli", fmt("console.log(`{}: ${{inspect({})}}`)", {i(1), rep(1)}))
    
    +table.insert(snippets, llt)
     table.insert(snippets, lld)
    -table.insert(snippets, llv)
     table.insert(snippets, lli)
    +table.insert(snippets, llw)
    +table.insert(snippets, lle)
    +table.insert(snippets, llf)
    +table.insert(snippets, llv)
    
     return snippets, autosnippets

--- snippets/javascript.lua
+++ snippets/javascript.lua
@@ -11,13 +11,23 @@ local rep = require("luasnip.extras").rep
 
 local snippets, autosnippets = {}, {}
 
-local lld= s("lld", fmt("console.log(`{}`)", {i(1)}))
+-- local lld= s("lld", fmt("console.log(`{}`)", {i(1)}))
+local llt= s("llt", fmt("logger.trace(`{}`)", {i(1)}))
+local lld= s("lld", fmt("logger.debug(`{}`)", {i(1)}))
+local lli= s("lli", fmt("logger.info(`{}`)", {i(1)}))
+local llw= s("llw", fmt("logger.warn(`{}`)", {i(1)}))
+local lle= s("lle", fmt("logger.error(`{}`)", {i(1)}))
+local llf= s("llf", fmt("logger.fatal(`{}`)", {i(1)}))
 local llv= s("llv", fmt("console.log(`{}: ${{{}}}`)", {i(1), rep(1)}))
-local lli= s("lli", fmt("console.log(`{}: ${{inspect({})}}`)", {i(1), rep(1)}))
+-- local lli= s("lli", fmt("console.log(`{}: ${{inspect({})}}`)", {i(1), rep(1)}))
 
+table.insert(snippets, llt)
 table.insert(snippets, lld)
-table.insert(snippets, llv)
 table.insert(snippets, lli)
+table.insert(snippets, llw)
+table.insert(snippets, lle)
+table.insert(snippets, llf)
+table.insert(snippets, llv)
 
 return snippets, autosnippets
 
