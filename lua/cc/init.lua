a3f55382261b566b9e6d48c5a556be8499daca43
Claudio Caseiro <ccaseiro@gmail.com>
2023-03-23 10:59:36 +0000

    -

--- lua/cc/init.lua
+++ lua/cc/init.lua
@@ -93,7 +93,7 @@ vim.keymap.set('!', '<C-a>', readline.dwim_beginning_of_line)
 vim.keymap.set('!', '<C-e>', readline.end_of_line)
 vim.keymap.set('!', '<M-d>', readline.kill_word)
 vim.keymap.set('!', '<M-BS>', readline.backward_kill_word)
-vim.keymap.set('!', '<C-w>', readline.unix_word_rubout)
+-- vim.keymap.set('!', '<C-w>', readline.unix_word_rubout)
 -- vim.keymap.set('!', '<C-k>', readline.kill_line)
 vim.keymap.set('!', '<C-u>', readline.dwim_backward_kill_line)
 
