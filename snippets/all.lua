c0674149947ba2890f161aa842f199ce991fec54
Claudio Caseiro <ccaseiro@gmail.com>
2023-03-29 09:56:09 +0100

    temp/experimental global snippets

--- snippets/all.lua
+++ snippets/all.lua
@@ -11,14 +11,35 @@ local rep = require("luasnip.extras").rep
 
 local snippets, autosnippets = {}, {}
 
-local myFirstSnippet = s("xxx", { t("New xHi again! This is my first snippet in Luasnip")})
+local myFirstSnippet = s("xxx", { 
+    t("Hello! This is my first snippet in Luasnip"),
+    i(1, " placeholder_text"),
+    i(2, " second"),
+    t(" this is another text node")
+})
+
 local expand = s("expand", { t("-- this is what was expanded!")})
 local req = s("req", fmt("local {} = require('{}')", {i(1, "default"), rep(1)}))
 
+-- local llx= s("llx", fmt("console.log(`{}: ${{{}}}`)", {i(1), rep(1)}))
+-- local llx= s("llx", fmt([[
+-- console.log(`{}: ${{{}}}`)
+-- ]], {
+--     i(1), rep(1)
+-- }))
+local fmt= s("fmt", fmt([[
+console.log(`{}: ${{{}}}. xxxx: {}`)
+]], {
+    i(1, "1"),
+    i(2, "3"),
+    c(3, {t("2"), t("this"), t("that")}),
+}))
 
-table.insert(snippets, myFirstSnippet)
+
+
+table.insert(autosnippets, myFirstSnippet)
 table.insert(snippets, expand)
-table.insert(snippets, ll)
 table.insert(snippets, req)
+table.insert(autosnippets, fmt)
 
 return snippets, autosnippets
