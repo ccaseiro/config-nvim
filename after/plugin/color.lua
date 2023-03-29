2d9e536094821e06f5128142432794c71eebeace
Claudio Caseiro <ccaseiro@gmail.com>
2023-03-29 09:55:50 +0100

    change colorscheme

--- after/plugin/color.lua
+++ after/plugin/color.lua
@@ -1,6 +1,7 @@
 vim.o.background = "dark" -- "dark" or "light" for light mode
+-- vim.o.background = "light" -- "dark" or "light" for light mode
 
--- vim.cmd[[colorscheme tokyonight]]
-vim.cmd[[colorscheme gruvbox]]
+vim.cmd[[colorscheme tokyonight]]
+-- vim.cmd[[colorscheme gruvbox]]
 -- vim.cmd[[colorscheme gruvbox-baby]]
 -- vim.cmd[[colorscheme onedark]]
