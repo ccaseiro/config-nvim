06ade19dc8fc8bd34e731c0a69b8d408d2ad8836
Claudio Caseiro <ccaseiro@gmail.com>
2023-03-30 01:32:26 +0100

    use last version of telescope

--- lua/cc/packer.lua
+++ lua/cc/packer.lua
@@ -71,8 +71,6 @@ return packer.startup(function(use)
 
     use {
         "nvim-telescope/telescope.nvim",
-        tag = "0.1.0",
-        -- or                            , branch = '0.1.x',
         requires = {
             {"nvim-lua/plenary.nvim"},
             -- { "nvim-telescope/telescope-fzf-native.nvim", run = "make" },
