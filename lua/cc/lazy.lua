fe13a5b94cd92f02d02c556cf7b2dbaa67a6f67f
Claudio Caseiro <ccaseiro@gmail.com>
2023-03-31 23:44:58 +0100

    fix leap: "x" not working in "V mode"

--- lua/cc/lazy.lua
+++ lua/cc/lazy.lua
@@ -23,8 +23,19 @@ local plugins = {
     },
     {
         "ggandor/leap.nvim",
-        config = function()
-            require("leap").add_default_mappings()
+        keys = {
+            { "s", mode = { "n", "x", "o" }, desc = "Leap forward to" },
+            { "S", mode = { "n", "x", "o" }, desc = "Leap backward to" },
+            { "gs", mode = { "n", "x", "o" }, desc = "Leap from windows" },
+        },
+        config = function(_, opts)
+            local leap = require("leap")
+            for k, v in pairs(opts) do
+                leap.opts[k] = v
+            end
+            leap.add_default_mappings(true)
+            vim.keymap.del({ "x", "o" }, "x")
+            vim.keymap.del({ "x", "o" }, "X")
         end,
     },
 
