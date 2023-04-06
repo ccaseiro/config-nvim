88a6b714d231162fe826e250a0ceeee1c78ba710
Claudio Caseiro <ccaseiro@gmail.com>
2023-04-06 12:13:16 +0100

    snippets

--- snippets/python.lua
+++ snippets/python.lua
@@ -5,11 +5,13 @@ local t = ls.t --> text node
 local fmt = require("luasnip.extras.fmt").fmt
 
 local assf = s("assf", { t("assert False") })
+local lli = s("lli", fmt('logger.info(f"{}")', { i(1, "") }))
 local lld = s("lld", fmt('logger.debug(f"{}")', { i(1, "") }))
 local llt = s("llt", fmt('logger.debug(f"{}")', { i(1, "") }))
 
 local snippets, autosnippets = {}, {}
 table.insert(autosnippets, assf)
+table.insert(autosnippets, lli)
 table.insert(autosnippets, lld)
 table.insert(autosnippets, llt)
 return snippets, autosnippets
