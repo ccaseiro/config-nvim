a11f0f781fc5c5e85c7c2f064a05775e001db31c
Claudio Caseiro <ccaseiro@gmail.com>
2023-04-30 12:43:45 +0100

    install nvim-notify

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -24,5 +24,9 @@ return {
             { "<c-f>", function() if not require("noice.lsp").scroll(4) then return "<c-f>" end end, silent = true, expr = true, desc = "Scroll forward", mode = {"i", "n", "s"} },
             { "<c-b>", function() if not require("noice.lsp").scroll(-4) then return "<c-b>" end end, silent = true, expr = true, desc = "Scroll backward", mode = {"i", "n", "s"}},
         },
+        dependencies = {
+            "MunifTanjim/nui.nvim",
+            "rcarriga/nvim-notify",
+        },
     },
 }
