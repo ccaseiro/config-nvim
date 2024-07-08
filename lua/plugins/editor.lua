2df954c59e492f11db4a2f77d59821cb2ce4a041
Claudio Caseiro <ccaseiro@gmail.com>
2024-07-08 17:52:31 +0100

    neo-tree

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,5 +1,116 @@
 return {
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        -- branch = "v2.x",
+        cmd = "Neotree",
+        keys = {
+            {
+                "<leader>fe",
+                function()
+                    require("neo-tree.command").execute({ toggle = true, dir = LazyVim.root() })
+                end,
+                desc = "Explorer NeoTree (Root Dir)",
+            },
+            {
+                "<leader>fE",
+                function()
+                    require("neo-tree.command").execute({ toggle = true, dir = vim.uv.cwd() })
+                end,
+                desc = "Explorer NeoTree (cwd)",
+            },
+            { "<leader>e", "<leader>fe", desc = "Explorer NeoTree (Root Dir)", remap = true },
+            { "<leader>E", "<leader>fE", desc = "Explorer NeoTree (cwd)", remap = true },
+            {
+                "<leader>ge",
+                function()
+                    require("neo-tree.command").execute({ source = "git_status", toggle = true })
+                end,
+                desc = "Git Explorer",
+            },
+            {
+                "<leader>be",
+                function()
+                    require("neo-tree.command").execute({ source = "buffers", toggle = true })
+                end,
+                desc = "Buffer Explorer",
+            },
+        },
+        -- _keys = {
+        --     {
+        --         "<leader>e",
+        --         function()
+        --             require("neo-tree.command").execute({ toggle = true, position = "float", dir = vim.loop.cwd() })
+        --         end,
+        --         desc = "Explorer NeoTree (cwd)",
+        --     },
+        --     {
+        --         "<leader>E",
+        --         function()
+        --             require("neo-tree.command").execute({
+        --                 toggle = true,
+        --                 position = "float",
+        --                 dir = vim.loop.os_homedir(),
+        --             })
+        --         end,
+        --         desc = "Explorer NeoTree (Home)",
+        --     },
+        --     {
+        --         "<leader>fe",
+        --         function()
+        --             require("neo-tree.command").execute({ position = "left", dir = vim.loop.cwd() })
+        --         end,
+        --         desc = "Explorer NeoTree (cwd)",
+        --     },
+        --     {
+        --         "<leader>fE",
+        --         function()
+        --             require("neo-tree.command").execute({ position = "left", dir = vim.loop.os_homedir() })
+        --         end,
+        --         desc = "Explorer NeoTree (Home)",
+        --     },
+        --     {
+        --         "<leader>ge",
+        --         function()
+        --             require("neo-tree.command").execute({ source = "git_status", toggle = true })
+        --         end,
+        --         desc = "Git Explorer",
+        --     },
+        --     {
+        --         "<leader>be",
+        --         function()
+        --             require("neo-tree.command").execute({ source = "buffers", toggle = true })
+        --         end,
+        --         desc = "Buffer Explorer",
+        --     },
+        -- },
 
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+            "MunifTanjim/nui.nvim",
+        },
+        config = function()
+            require("neo-tree").setup({
+                filesystem = {
+                    bind_to_cwd = false,
+                    follow_current_file = true,
+                },
+                window = {
+                    mappings = {
+                        ["<space>"] = "none",
+                    },
+                },
+                default_component_configs = {
+                    indent = {
+                        with_expanders = true, -- if nil and file nesting is enabled, will enable expanders
+                        expander_collapsed = "",
+                        expander_expanded = "",
+                        expander_highlight = "NeoTreeExpander",
+                    },
+                },
+            })
+        end,
+    },
     -- Automatically highlights other instances of the word under your cursor.
     -- This works with LSP, Treesitter, and regexp matching to find the other
     -- instances.
