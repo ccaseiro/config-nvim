f555eda27c32f6357dcb4195192fe7f9d6e46f3c
Claudio Caseiro <ccaseiro@gmail.com>
2024-07-08 17:54:39 +0100

    lazy-lock

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -151,25 +151,25 @@ return {
         },
     },
     { "tpope/vim-fugitive" },
-    {
-        "SuperBo/fugit2.nvim",
-        opts = {
-            width = 100,
-        },
-        dependencies = {
-            "MunifTanjim/nui.nvim",
-            "nvim-tree/nvim-web-devicons",
-            "nvim-lua/plenary.nvim",
-            {
-                "chrisgrieser/nvim-tinygit", -- optional: for Github PR view
-                dependencies = { "stevearc/dressing.nvim" },
-            },
-        },
-        cmd = { "Fugit2", "Fugit2Graph" },
-        keys = {
-            { "<leader>gf", mode = "n", "<cmd>Fugit2<cr>" },
-        },
-    },
+    -- {
+    --     "SuperBo/fugit2.nvim",
+    --     opts = {
+    --         width = 100,
+    --     },
+    --     dependencies = {
+    --         "MunifTanjim/nui.nvim",
+    --         "nvim-tree/nvim-web-devicons",
+    --         "nvim-lua/plenary.nvim",
+    --         {
+    --             "chrisgrieser/nvim-tinygit", -- optional: for Github PR view
+    --             dependencies = { "stevearc/dressing.nvim" },
+    --         },
+    --     },
+    --     cmd = { "Fugit2", "Fugit2Graph" },
+    --     keys = {
+    --         { "<leader>gf", mode = "n", "<cmd>Fugit2<cr>" },
+    --     },
+    -- },
     {
         -- optional: for diffview.nvim integration
         "sindrets/diffview.nvim",
